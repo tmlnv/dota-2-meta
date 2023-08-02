265e1fd6e7d5459ea5f49fc6ee311cb2c973e7ef
tmlnv <tmlnv@protonmail.com>
2023-08-02 21:42:04 +0300

    Got top 5 heroes with names stratz GraphQL API
    Added role selector functionality
    
    Updated footer media query
    
    Added mmr selector working, refactored to single API fetch on load
    
    UPD package, webpack, startz api

--- dev/stratz.js
+++ dev/stratz.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
+
 const url = "https://api.stratz.com/graphql";
+// const api_token = process.env.STRATZ_API_KEY;
 const api_token = '';
 const headers = {
   "Authorization": `Bearer ${api_token}`,
@@ -45,7 +49,6 @@ function fetchData(query) {
 
 async function getHeroStats() {
     const response = await fetchData(query);
-    console.log(response)
     return response["data"]["heroStats"]["winDay"];
 }
 
@@ -53,7 +56,6 @@ async function getWinCountMatchCount() {
     const heroWinCountMatchCount = {};
     const heroStats = await getHeroStats();
     for (let hero of heroStats) {
-        console.log(hero);
         if (heroWinCountMatchCount[hero['heroId']] === undefined) {
           heroWinCountMatchCount[hero['heroId']] = {'winCount': hero['winCount'], 'matchCount': hero['matchCount']};
         } else {
@@ -66,10 +68,43 @@ async function getWinCountMatchCount() {
 
     }
     
-    console.log(heroWinCountMatchCount);
     return heroWinCountMatchCount;
 }
 
+const queryHeroData = `query {
+    constants{
+      heroes(language: ENGLISH) {
+        id,
+        name
+      }
+    }
+  }`;
+
+async function getHeroData() {
+    const responce = await fetchData(queryHeroData);
+    for (let item of responce["data"]["constants"]["heroes"]) {
+    }
+    return responce["data"]["constants"]["heroes"];
+}
+
+
+async function getTopHeroes() {
+    const heroIdsNames = await getHeroData();
+    const topHeroes = await getWinCountMatchCount();
+    const topFiveHeroes = Object.entries(topHeroes).sort(
+        ([, heroData], [, heroData2]) =>  heroData2.winRate - heroData.winRate
+    ).slice(0, 5).reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    const namedTop = {}
+    for (elem of heroIdsNames) {
+        if (elem.id in topFiveHeroes) {
+            namedTop[elem.name] = topFiveHeroes[elem.id].winRate;
+        }
+    }
+    console.log(namedTop);
+    return namedTop;
+}
+
 (async () => {
-  await getWinCountMatchCount();
+    await getTopHeroes();
 })()
