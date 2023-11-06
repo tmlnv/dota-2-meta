941ea93f7f956e81aaa5071e2eab42312ee869bd
tmlnv <tmlnv@protonmail.com>
2023-11-06 23:48:55 +0300

    replaced =>{} with =>() with implicit returns

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,14 +1,12 @@
-const Loading: React.FC = () => {
-  return (
-    <>
-      <div className="loader">
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-      </div>
-    </>
-  );
-};
+const Loading: React.FC = () => (
+  <>
+    <div className="loader">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  </>
+);
 
 export default Loading;
