80fbc7a8567f9c5748e52434451d55bf3122f194
tmlnv <tmlnv@protonmail.com>
2023-11-16 23:17:32 +0300

    Added tests for dropdowns, fixed bug with using incorrect ALL_ROLES so no Escape role was used

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,8 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+import { ALL_ROLES, MMRMAPPING } from '../src/components/config';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://dota-2-meta.vercel.app/');
+test.beforeEach( async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+});
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Dota 2 Meta/);
+test.describe('all', () => {
+    test('has title', async ({ page }) => {
+        // Expect a title "to contain" a substring.
+        await expect(page).toHaveTitle(/Dota 2 Meta/);
+
+        await new Promise(res => setTimeout(res, 2000));
+    });
+
+    for (let role of ALL_ROLES) {
+        test(`Role dropdown - ${role}`, async ({ page }) => {
+            await page.locator('#roleDropdown div').click();
+            await page.getByText(role).click();
+            
+            expect(page.locator('div').filter({ hasText: role })).toBeVisible;
+        });
+    };
+
+    for (let mmr in MMRMAPPING) {
+        test(`Mmr dropdown - ${mmr}`, async ({ page }) => {
+            await page.locator('#mmrDropdown div').click();
+            await page.getByText(mmr).click();
+            
+            expect(page.locator('div').filter({ hasText: mmr })).toBeVisible;
+        });
+    };
 });
