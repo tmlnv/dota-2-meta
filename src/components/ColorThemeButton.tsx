aa83901c76cb0a986c1ed5c643bfb1b83ee23acf
tmlnv <tmlnv@protonmail.com>
2024-05-10 14:12:13 +0300

    Theme toggle button

--- src/components/ColorThemeButton.tsx
+++ src/components/ColorThemeButton.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ThemeSwitch, ThemeCheckbox, Label, ToggleSlider, Icon } from "../styles/themeToggle/themeToggle";
 import StyledButton from "../styles/colorThemeButton";
 
 const ColorThemeToggleButton = () => {
@@ -27,9 +28,36 @@ const ColorThemeToggleButton = () => {
   };
 
   return (
-    <StyledButton onClick={toggleTheme}>
-      Switch Color Theme
-    </StyledButton>
+    // <StyledButton onClick={toggleTheme}>
+    // Switch Color Theme
+    // </StyledButton>
+    <ThemeSwitch>
+      <ThemeCheckbox id="theme-checkbox" onChange={toggleTheme} checked={dark} />
+      <Label htmlFor="theme-checkbox">
+        <ToggleSlider style={{ left: dark ? '1.6em' : '0.1em', backgroundColor: dark ? '#212121' : '#f2f2f2' }} />
+        <span>
+          <Icon
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="icon"
+          >
+            {/* Icon path for moon or sun */}
+            {dark ? (
+              <path
+                fillRule="evenodd"
+                d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+                clipRule="evenodd"
+              />
+            ) : (
+              <path
+                d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
+              />
+            )}
+          </Icon>
+        </span>
+      </Label>
+    </ThemeSwitch>
   );
 };
 
---
+++ src/styles/themeToggle/themeToggle.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const ThemeSwitch = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  direction: rtl;
+`;
+
+const ThemeCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  display: none;
+`;
+
+const Label = styled.label`
+  font-size: 2rem;
+  height: 1em;
+  width: 2.5em;
+  border-radius: 0.25em;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  background-color: #cecece;
+  position: relative;
+
+  &:active {
+    transform: scale(0.85);
+    transition: transform 0.2s;
+  }
+`;
+
+const ToggleSlider = styled.div`
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: inherit;
+  position: absolute;
+  top: 0.1em;
+  left: 0.1em;
+  z-index: 10;
+  transition: 0.5s cubic-bezier(1, 0.33, 0.11, 1.34);
+  background-color: #f2f2f2;
+`;
+
+const Icon = styled.svg`
+  display: inline-block;
+  height: 1em;
+  width: 1em;
+  padding: 0.15em;
+  box-sizing: border-box;
+`;
+
+export { ThemeSwitch, ThemeCheckbox, Label, ToggleSlider, Icon };
+
